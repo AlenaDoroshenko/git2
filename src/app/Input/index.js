1a5d7a4eb85cae47e50f74d55d469eb79b0b1e88
Maelstromm <velsamm@yandex.ru>
2020-11-11 20:13:48 +0300

    init calculator frontend

---
+++ src/app/Input/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = () => {
+    return (
+        <div className='p-20'>
+            <input name='calc-input' />
+        </div>
+
+    )
+}
+
+export default Input
---
+++ src/app/Output/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Output = () => {
+    return (
+        <div className='output p-20'>
+            0
+        </div>
+    )
+}
+
+export default Output
